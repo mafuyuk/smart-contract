37a9c40b9ba3eb7135e3d0df4b6d633718ab4286
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-04-18 19:12:00 +0900

    Added infra deploy

--- my-token/truffle.js
+++ my-token/truffle.js
@@ -1,4 +1,15 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = process.env.ROPSTEN_NEMONIC;
+var accessToken = process.env.INFURA_ACCESS_TOKEN;
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+      ropsten: {
+          provider: function() {
+              return new HDWalletProvider(mnemonic, "https://ropsten.infra.io/" + accessToken);
+          },
+          network_id: 3,
+          gas: 500000,
+      }
+  }
 };
