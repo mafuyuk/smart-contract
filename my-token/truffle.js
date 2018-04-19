702ee305ff967ecec27522de850da65fea4501da
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-04-20 00:51:08 +0900

    Added development network

--- my-token/truffle.js
+++ my-token/truffle.js
@@ -4,12 +4,17 @@ var accessToken = process.env.INFURA_ACCESS_TOKEN;
 
 module.exports = {
     networks: {
+        development: {
+            host: "localhost",
+            port: 8545,
+            network_id: 10
+        },
         ropsten: {
             provider: function() {
                 return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + accessToken);
             },
-            network_id: "*",
-            gas: 2000000
+            network_id: 3,
+            gas: 500000
         }
     }
 };
