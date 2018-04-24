da947065da8c47227d7a9aa1f52af166847f4cd5
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-04-24 10:57:54 +0900

    Added ABI link

--- hello-solidity/index.js
+++ hello-solidity/index.js
@@ -2,13 +2,15 @@ const Web3 = require('web3');
 const solc = require('solc');
 const fs = require('fs');
 
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+const web3 = new Web3(new Web3.providers.HttpProvider("http://0.0.0.0:8545"));
 
 // SolidityをCompile
 const sourceCode = fs.readFileSync('Greetings.sol').toString();
 const compiledCode = solc.compile(sourceCode);
 
 // ABIを取得
+// https://solidity.readthedocs.io/en/develop/abi-spec.html
+// https://ja.wikipedia.org/wiki/Application_Binary_Interface
 const contractABI = JSON.parse(compiledCode.contracts[':Greetings'].interface);
 const greetingsContract = web3.eth.contract(contractABI);
 
